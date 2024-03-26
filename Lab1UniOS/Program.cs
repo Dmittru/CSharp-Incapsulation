40a189447eb22c1150d56054fafd0c5da22ca24a
CERF <limbooo689@gmail.com>
2024-03-26 18:43:45 +0300

    c#

---
+++ Lab1UniOS/Program.cs
@@ -0,0 +1,40 @@
+﻿using Lab1UniOS;
+using System;
+using System.Numerics;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лаб 1");
+
+            Complex a = new() { Real = 3, Imag = 0.5 };
+            Complex b = new() { Real = 2, Imag = 0.8 };
+
+            a.Add(b);
+
+            b.Subtract(a);
+
+            Console.WriteLine("a = {0} + {1}i", a.Real, a.Imag);
+            Console.WriteLine("b = {0} + {1}i", b.Real, b.Imag);
+
+            Console.WriteLine("Класс студентов");
+
+            Student student1 = new Student("Иван", "Мужчина", "Коричневый", 20);
+            Student student2 = new Student("Алиса", "Женщина", "Блонд", 22);
+            Student student3 = new Student("Петр", "Мужчина", "Рыжий", 25);
+
+            Console.WriteLine("Информация о студенте 1:");
+            Console.WriteLine(student1.ToString());
+
+            Console.WriteLine("Информация о студенте 2:");
+            Console.WriteLine(student2.ToString());
+
+            Student sum = Student.AddStudents(student1, student2, student3);
+            Console.WriteLine("Результат сложения студентов:");
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
---
+++ Lab1UniOS/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1UniOS
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string HairColor { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, string gender, string hairColor, int age)
+        {
+            Name = name;
+            Gender = gender;
+            HairColor = hairColor;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Gender: {Gender}, Hair Color: {HairColor}, Age: {Age}";
+        }
+
+        public static Student AddStudents(params Student[] students)
+        {
+            if (students == null || students.Length == 0)
+                throw new ArgumentException("At least one student must be provided.");
+
+            string combinedName = string.Join(" & ", students.Select(s => s.Name));
+            string combinedGender = string.Join(", ", students.Select(s => s.Gender).Distinct());
+            string combinedHairColor = string.Join(", ", students.Select(s => s.HairColor).Distinct());
+            int combinedAge = (int)students.Average(s => s.Age); // Средний возраст
+
+            return new Student(combinedName, combinedGender, combinedHairColor, combinedAge);
+        }
+    }
+}
